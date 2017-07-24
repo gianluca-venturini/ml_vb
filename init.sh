430100a6bf60685564b1914e882f56fff6965694
ran <ran@dropbox.com>
2017-07-24 16:27:10 +0300

    add ml scripts for text overlapping, data creation, model training, and bug detection with model

--- init.sh
+++ init.sh
@@ -5,11 +5,15 @@ source ./bin/activate
 pip install tensorflow
 pip install selenium
 pip install Pillow
+<<<<<<< HEAD
 pip install numpy
 pip install sklearn
 pip install scipy
 pip install keras
 pip install h5py
+=======
+pip install keras
+>>>>>>> add ml scripts for text overlapping, data creation, model training, and bug detection with model
 # Download geckodriver for Mac
 curl -L https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-macos.tar.gz | tar xz -C bin
 # Download Firefox for Mac
---
+++ text_ovelapping_model/detector/keras_verify_image.py
@@ -0,0 +1,131 @@
+import os
+import itertools
+
+
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+import os
+import pickle
+
+# first remove bacground than attach model detectors per image size
+# each model use its color to mark detected bugs
+
+path = '/Users/ran/Documents/ML_bad_lf/indent/'
+path_photo  = path +"test"
+scaler_path = path+"scaler"
+model_name = 'cropped-text12'
+keras_model_path = path+"model"+model_name+".h5"
+#
+# window_size = 12
+# step_size = 5
+
+# load models
+background_model = load_model(keras_model_path)
+with open(scaler_path + model_name, "rb") as f:
+    background_scaler = pickle.load(f)
+
+inner_model_name = "cropped-overlap10"
+with open(scaler_path + inner_model_name, "rb") as f:
+    scaler_10 = pickle.load(f)
+model_10 = load_model(path+"model" + inner_model_name + ".h5")
+
+
+def get_models(window_size):
+    if window_size == 0:
+        return (scaler_10, model_10, 10, 1, 0.9999999, 15,True)
+
+
+def check_image(img, pixels,
+                scaler, model,
+                window_size,
+                step_size,
+                x, y,
+                convert_to_l=True,
+                tresh_hold=0.99,
+                mark_color=100,
+                smaller_than_treshhold=True,
+                ):
+    n, m = img.size
+    s = set()
+    colored = []
+    for i, j in itertools.product(range(0, n - window_size, step_size),
+                                  range(0, m - window_size, step_size)):
+        print '.',
+
+        cropped_img = img.crop((i, j, i + window_size, j + window_size))
+        if convert_to_l:
+            cropped_img = cropped_img.convert('L')
+
+        arr_orig = np.array(cropped_img).ravel()
+
+        arr = scaler.transform(arr_orig).tolist()
+        proba = model.predict(np.array([arr]))[0]
+        r = window_size/2
+        if (proba > tresh_hold)== smaller_than_treshhold:
+            colored.append((i,j))
+            for k, l in itertools.product(range(-r, r), range(-r, r)):
+                s.add(str(proba))
+                if mark_color is not 0:
+                    pixels[x + i + int(window_size / 2) + k, y + j + int(window_size / 2) + l] = (
+                255, mark_color, int(200 * proba))
+            # get scalar and model per image size
+            params = get_models(window_size)
+            if params:
+                sc, mo, win_s, step_s, t_hold, m_color, smtt= params
+                check_image(cropped_img, pixels, sc, mo, win_s, step_s, x + i, y + j,
+                            convert_to_l=False,
+                            tresh_hold=t_hold,
+                            mark_color=100,
+                            smaller_than_treshhold=smtt
+                )
+
+    print s
+    print len(colored)
+    print colored
+    only_rects(colored, step_size)
+    return colored
+
+
+# leave only edge points of colored lines
+def only_rects(line, step):
+    rem = 0
+    for size in range(1,len(line)):
+        size = size- rem
+        i , j = line[size]
+        first = size % 2 != 0
+        last = size - 1
+        prev_i, prev_j = line[last]
+        same_line = i == prev_i
+
+        if first:
+            if not same_line:
+                del line[last]
+                rem = rem + 1
+            elif j > 10 + prev_j + step:
+                del line[last]
+                rem = rem +1
+        else:
+            if j <= 10 + prev_j + step:
+                del line[last]
+                rem = rem + 1
+
+#preparing photo windows from given photos. prints the name of each file and each dot represents a cropped photo
+listing = os.listdir(path_photo)
+for photo in listing:
+    try:
+        img = Image.open(path_photo + '/' + photo)
+        img_new = img.copy()
+        pixels = img_new.load()
+        c_list = check_image(img, pixels, background_scaler, background_model, 12, 12, 0, 0)
+        with open(path_photo+ "/res-img/"+ "img_" + photo, "wb") as f:
+            pickle.dump(c_list, f)
+        print "|||||||||||||||||||||||||"
+        print (len(c_list))
+        print "|||||||||||||||||||||||||"
+        img_new.show()
+        img_new.save(path_photo + "/res-img/" + photo + "_t_" + '.png')
+    except IOError as e:
+        # raise
+        print e
+
